7de5c68d34568b5d9e4038ce0f3fc59927cdf55b
“msultan2” <mohamed.sultan1581@gmail.com>
2017-02-20 11:49:35 +0000

    Database.java changed the constructor to private.

--- src/main/java/com/mycompany/db/Database.java
+++ src/main/java/com/mycompany/db/Database.java
@@ -35,7 +35,7 @@ public enum Database {
   private final String password;
   private final DatabaseConnectionPool pool;
 
-  Database(final String jdbcUrl, final String username, final String password) {
+  private Database(final String jdbcUrl, final String username, final String password) {
     this.url = jdbcUrl;
     this.username = username;
     this.password = password;
